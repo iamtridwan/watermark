4218b50dacd1e9f8b1d4d230812762c4ea5bc415
Tridwan <iamtridwan@gmail.com>
2022-05-28 14:31:39 +0100

    uploads and result

--- src/components/contact/contactHeader/ContactHeader.tsx
+++ src/components/contact/contactHeader/ContactHeader.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "./contactHead.css";
 
-const Bounce = require("react-reveal/Bounce");
-const Fade = require("react-reveal/Fade");
+const Pulse = require("react-reveal/Pulse");
+
 
 const ContactHeader = () => {
   return (
     <div className="contact-head">
-      <Bounce left>
+      <Pulse left>
         <div className="head-content">
           <h1>Contact Us</h1>
           <p>
             Reach out to us below, we are always available for your service.
           </p>
         </div>
-      </Bounce>
+      </Pulse>
     </div>
   );
 };
--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import Integration from '../components/Home/homeIntegration/Integration';
+import ResultHeader from '../components/result/ResultHeader';
+import Updates from '../components/Home/update/Updates';
 
 const Result = () => {
   return (
-    <div>Result</div>
+    <>
+    <ResultHeader />
+    <Integration />
+    <Updates />
+    </>
   )
 };
 
